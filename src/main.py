41ee38f0abee5cacac0e6b65d60054cff535f324
Harsh Jailwal <hjailwal42@gmail.com>
2026-08-20 00:17:40 +0530

    guess the number game

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
-print("Hello world")
-print ("to my second git commit not it is first only")
-lis =[1,2,3,4]
+import random 
+g_no =  random.randint(1,50)
+print("hello welcome to the number guessing game")
+print(g_no)
+num = int(input("Guess the number"))
+while True:
+   
+    if num>g_no:
+        print("number is very high")
+    elif num<g_no:
+        print("number is very less")
+    else :
+        print("hey finally you guessed the number correct hurrah")
+        break
+    num = int(input("guess again "))
+    
+
